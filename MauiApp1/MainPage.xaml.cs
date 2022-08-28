291bc9154371654fbb84d3d099b5aa7bcd497b9c
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-08-28 00:51:47 -0700

    Agregar archivos de proyecto.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		
+		if (count == 1)
+			CounterBtn.Text = $"Hiciste click {count} vez";
+		else
+			CounterBtn.Text = $"Hiciste click {count} veces";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
